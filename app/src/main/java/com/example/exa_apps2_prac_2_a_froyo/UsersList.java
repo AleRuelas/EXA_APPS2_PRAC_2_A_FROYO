460a1199d49733d6fee9efc1b4218a4a0afe21da
AleRuelas <aleruelas03@gmail.com>
2020-04-24 22:46:12 -0600

    Interface creation

---
+++ app/src/main/java/com/example/exa_apps2_prac_2_a_froyo/UsersList.java
@@ -0,0 +1,14 @@
+package com.example.exa_apps2_prac_2_a_froyo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsersList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_list);
+    }
+}
